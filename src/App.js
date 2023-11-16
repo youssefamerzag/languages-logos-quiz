fef0269ca75be33c8fae13e35399d462d3131859
youssef <amerzag.youssef4@gmail.com>
2023-11-16 14:36:14 +0100

    add  more languages and add score and if the answer is incorrect refresh the window

--- src/App.js
+++ src/App.js
@@ -6,22 +6,37 @@ import html from './imgs/html.png'
 import redux from './imgs/redux.png'
 import swift from './imgs/swift.png'
 import mongodb from './imgs/mongodb.png'
+import Reactlogo from './imgs/reactjs.png';
+import ruby from './imgs/ruby.png'
+import typescript from './imgs/typescript.png'
+import nodejs from './imgs/nodejs.png'
+
 
 
 function App() {
   const imgs = [
-    {id : 3 , source : swift},
-    {id : 0 , source : html},
-    {id : 2 , source : mongodb },
-    {id : 1 , source : redux}
+    {id : 7 , source : typescript},
+    {id : 2 , source : swift},
+    {id : 1 , source : html},
+    {id : 4 , source : Reactlogo},
+    {id : 5 , source : nodejs},
+    {id : 0 , source : mongodb },
+    {id : 3 , source : redux},
+    {id : 6 , source : ruby},
+
+
 
   ]
 
   const words = [
-    {id : 2 , answer : "mongodb"},
-    {id : 0 , answer : "html"},
-    {id : 3 , answer : "swift"},
-    {id : 1 , answer : "redux"},
+    {id : 0 , answer : "mongodb"},
+    {id : 1 , answer : "html"},
+    {id : 2 , answer : "swift"},
+    {id : 3 , answer : "redux"},
+    {id : 4 , answer : "React JS"},
+    {id : 5 , answer : "Node JS"},
+    {id : 6 , answer : "Ruby"},
+    {id : 7 , answer : "Typescript"},
   ]
   
 
@@ -30,6 +45,7 @@ function App() {
 
   const [choosingimg , setchoosingimg] = useState()
   const [choosingword , setchoosingword] = useState()
+  const [score , setscore] = useState(0)
   
 
   const imganswer =(id)=> {
@@ -40,29 +56,36 @@ function App() {
 
   const wordanswer =(id)=> {
     setchoosingword(words[id].id)
+    wordcolor.current[words[id].id].current.style.backgroundColor = 'lightblue'
     console.log(choosingword)
   }
 
   useEffect(() => {
     if(choosingimg !== undefined && choosingword !== undefined){
       if(choosingimg === choosingword){
+        setscore(score + 1)
+        if(score === 8 - 1) {alert('You Win')}
         wordcolor.current[choosingword].current.style.backgroundColor = 'lightgreen';
         imgbackground.current[choosingimg].current.style.backgroundColor = 'lightgreen'
         setchoosingimg()
         setchoosingword()
       }else{
+        alert('You Lose!!!')
+        window.location.reload();
         wordcolor.current[choosingword].current.style.backgroundColor = 'red'
         imgbackground.current[choosingimg].current.style.backgroundColor = 'red'
         setchoosingimg()
         setchoosingword()
       }
+
     }
   },[imganswer, wordanswer])
 
-
+ 
   return (
     <div className="App">
-      <h1>project</h1>
+      <h1>Quiz</h1>
+      <h1>Your Score : {score} / 8</h1>
       <div id='imgs'>
         {imgs.map((img, index) => 
             <img ref={imgbackground.current[img.id]}  onClick={() => imganswer(index)} key={index} src={img.source} ></img>
