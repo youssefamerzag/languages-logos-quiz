7de0e375037c14f762673fba7b065c03d78b7f2d
youssef <amerzag.youssef4@gmail.com>
2023-11-17 15:57:27 +0100

    change imgs and words places every time you play

--- src/App.js
+++ src/App.js
@@ -11,66 +11,65 @@ import ruby from './imgs/ruby.png'
 import typescript from './imgs/typescript.png'
 import nodejs from './imgs/nodejs.png'
 
+const imgs = [
+  { id: 0, source: mongodb },
+  { id: 1, source: html },
+  { id: 2, source: swift },
+  { id: 3, source: redux },
+  { id: 4, source: Reactlogo },
+  { id: 5, source: nodejs },
+  { id: 6, source: ruby },
+  { id: 7, source: typescript },
+]
+const words = [
+  { id: 0, answer: "mongodb" },
+  { id: 1, answer: "html" },
+  { id: 2, answer: "swift" },
+  { id: 3, answer: "redux" },
+  { id: 4, answer: "React JS" },
+  { id: 5, answer: "Node JS" },
+  { id: 6, answer: "Ruby" },
+  { id: 7, answer: "Typescript" },
+]
+
+const randomWord = [...words].sort(() => Math.random() - 0.5)
+const randomImgs = [...imgs].sort(() => Math.random() - 0.5)
 
 function App() {
-  const imgs = [
-    {id : 7 , source : typescript},
-    {id : 2 , source : swift},
-    {id : 1 , source : html},
-    {id : 4 , source : Reactlogo},
-    {id : 5 , source : nodejs},
-    {id : 0 , source : mongodb },
-    {id : 3 , source : redux},
-    {id : 6 , source : ruby},
-  ]
 
-  const words = [
-    {id : 0 , answer : "mongodb"},
-    {id : 1 , answer : "html"},
-    {id : 2 , answer : "swift"},
-    {id : 3 , answer : "redux"},
-    {id : 4 , answer : "React JS"},
-    {id : 5 , answer : "Node JS"},
-    {id : 6 , answer : "Ruby"},
-    {id : 7 , answer : "Typescript"},
-  ]
-  
 
   const wordcolor = useRef(words.map(() => React.createRef()))
   const imgbackground = useRef(imgs.map(() => React.createRef()))
 
-  const [choosingimg , setchoosingimg] = useState()
-  const [choosingword , setchoosingword] = useState()
-  const [score , setscore] = useState(0)
+  const [choosingimg, setchoosingimg] = useState()
+  const [choosingword, setchoosingword] = useState()
+  const [score, setscore] = useState(0)
 
 
-  
-
-  const imganswer =(id)=> {
+  const imganswer = (id) => {
     setchoosingimg(imgs[id].id)
     imgbackground.current[imgs[id].id].current.style.backgroundColor = 'lightblue'
     if (choosingimg !== undefined) {
       imgbackground.current[choosingimg].current.style.backgroundColor = null;
-    } 
+    }
     console.log(choosingimg)
   }
 
-  
-  const wordanswer =(id)=> {
+
+  const wordanswer = (id) => {
     setchoosingword(words[id].id)
-    if (wordcolor !== undefined) {
-      wordcolor.current[words[id].id].current.style.backgroundColor = null
-    }else {
-      wordcolor.current[words[id].id].current.style.backgroundColor = 'lightblue'
+    wordcolor.current[words[id].id].current.style.background = 'lightblue';
+    if (choosingword !== undefined) {
+      wordcolor.current[choosingword].current.style.background = null
     }
-    console.log(choosingword)
   }
 
+
   useEffect(() => {
-    if(choosingimg !== undefined && choosingword !== undefined){
-      if(choosingimg === choosingword){
+    if (choosingimg !== undefined && choosingword !== undefined) {
+      if (choosingimg === choosingword) {
         setscore(score + 1)
-        if(score === 8 - 1) {
+        if (score === 8 - 1) {
           alert('You Win!!')
           window.location.reload()
         }
@@ -78,7 +77,7 @@ function App() {
         imgbackground.current[choosingimg].current.style.backgroundColor = 'lightgreen'
         setchoosingimg()
         setchoosingword()
-      }else{
+      } else {
         alert('You Lose!!!')
         window.location.reload();
         wordcolor.current[choosingword].current.style.backgroundColor = 'red'
@@ -88,22 +87,22 @@ function App() {
       }
 
     }
-  },[imganswer, wordanswer])
+  }, [imganswer, wordanswer])
 
- 
+
   return (
     <div className="App">
       <h1>Quiz</h1>
       <h1>Your Score : {score} / 8</h1>
       <div id='imgs'>
-        {imgs.map((img, index) => 
-            <img ref={imgbackground.current[img.id]}  onClick={() => imganswer(index)} key={index} src={img.source} ></img>
-          )}
+        {randomImgs.map((img, index) =>
+          <img ref={imgbackground.current[img.id]} onClick={() => imganswer(img.id)} key={img.id} id={img.id} src={img.source} ></img>
+        )}
       </div>
       <div id='words'>
-        {words.map((word , index) => 
-            <p ref={wordcolor.current[word.id]} onClick={() => wordanswer(index)} key={index} id={word.id}>{word.answer}</p>
-          )}
+        {randomWord.map((word, index) =>
+          <p ref={wordcolor.current[word.id]} onClick={() => wordanswer(word.id)} key={word.id} id={word.id}>{word.answer}</p>
+        )}
       </div>
     </div>
   );
